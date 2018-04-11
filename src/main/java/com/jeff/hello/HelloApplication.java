ddbea95cb99b84ed35356bb6f9ffc47951687d54
vq4988nx <vq4988nx@go.minneapolis.edu>
2018-04-10 22:04:49 -0500

    Initial commit of Hello Spring

---
+++ src/main/java/com/jeff/hello/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.jeff.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class HelloApplication {
+
+
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Spring";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
